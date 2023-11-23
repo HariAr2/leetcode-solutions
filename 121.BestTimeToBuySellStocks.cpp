662f3d78955c056caa9f1ec2f6b9ca1d4b7211c2
Hari Aravindh <127916422+HariAr2@users.noreply.github.com>
2023-11-23 21:51:51 +0530

    Update 121.BestTimeToBuySellStocks.cpp

--- 121.BestTimeToBuySellStocks.cpp
+++ 121.BestTimeToBuySellStocks.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int min=INT_MAX;
-        int maxp=0;
+        int maxp=0; 
         for(int i=0;i<prices.size();i++){
             if(prices[i]<min){
                 min=prices[i];
